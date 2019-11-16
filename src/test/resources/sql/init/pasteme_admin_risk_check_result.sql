ce028417cb1e6cff6b81665c02323d142c66164d
Lucien Shui <lucien@lucien.ink>
2019-11-16 16:40:28 +0800

    Add sql

---
+++ src/test/resources/sql/init/pasteme_admin_risk_check_result.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `pasteme_admin_risk_check_result`
+(
+    `id`     BIGINT UNSIGNED NOT NULL COMMENT '主键' PRIMARY KEY AUTO_INCREMENT,
+    `key`    BIGINT UNSIGNED NOT NULL COMMENT 'Paste 主键',
+    `type`   INT             NOT NULL COMMENT '风控检测类型',
+    `result` TEXT            NOT NULL COMMENT '检测结果',
+    KEY `idx_key` (`key`),
+    KEY `idx_key_type` (`key`, `type`)
+)
+    COMMENT '风控检测结果'
+    COLLATE = utf8mb4_general_ci
+    ENGINE = Innodb
+    DEFAULT CHARSET = utf8mb4;
---
+++ src/test/resources/sql/init/pasteme_admin_risk_state.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `pasteme_admin_risk_state`
+(
+    `id`    BIGINT UNSIGNED NOT NULL COMMENT '主键' PRIMARY KEY AUTO_INCREMENT,
+    `key`   BIGINT UNSIGNED NOT NULL COMMENT 'Paste 主键',
+    `type`  INT             NOT NULL COMMENT '类型',
+    `state` INT             NOT NULL COMMENT '状态',
+    KEY `idx_key` (`key`)
+) COLLATE = utf8mb4_general_ci
+  ENGINE = Innodb
+  DEFAULT CHARSET = utf8mb4;
