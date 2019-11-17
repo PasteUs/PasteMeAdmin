c576854207e159ae764375b46e9ff6a574d497fe
Lucien Shui <lucien@lucien.ink>
2019-11-18 00:17:04 +0800

    Fit for mysql 5.5

--- src/main/resources/sql/init/pasteme_admin_dictionary.sql
+++ src/main/resources/sql/init/pasteme_admin_dictionary.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `pasteme_admin_dictionary`
 (
     `id`         BIGINT UNSIGNED NOT NULL COMMENT '主键' PRIMARY KEY AUTO_INCREMENT,
     `type`       INT             NOT NULL COMMENT '词典类型',
-    `dictionary` JSON            NOT NULL COMMENT '词典，不兼容 5.7 以前的 MySQL Server',
+    `dictionary` TEXT            NOT NULL COMMENT '词典，为了兼容 MySQL 5.5 版本使用 TEXT 替代 JSON',
     KEY `idx_type` (`type`)
 )
     COMMENT '存储不同类型的词典'
