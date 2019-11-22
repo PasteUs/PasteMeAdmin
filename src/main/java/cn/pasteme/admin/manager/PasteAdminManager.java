391c649360babc26904e49da19558bb305764ea1
1059438020 <1502010425@s.upc.edu.cn>
2019-11-22 20:42:41 +0800

    Complete the Manager Interface of access count

--- src/main/java/cn/pasteme/admin/manager/PasteAdminManager.java
+++ src/main/java/cn/pasteme/admin/manager/PasteAdminManager.java
@@ -3,6 +3,8 @@ package cn.pasteme.admin.manager;
 import cn.pasteme.admin.enumeration.RiskStateDoState;
 import cn.pasteme.admin.enumeration.RiskStateDoType;
 
+import java.util.Date;
+
 /**
  * @author Lucien
  * @version 1.2.2
@@ -27,4 +29,40 @@ public interface PasteAdminManager {
      * @return boolean
      */
     boolean changeTypeAndStateByKey(Long key, RiskStateDoType type, RiskStateDoState state);
+
+    /**
+     * 计数单条 Paste 的周期（年/月/日）访问次数
+     * 用 dateConverterUtil 将日期根据 type 转换为起始时间
+     *
+     * @param key 主键
+     * @param date 日期
+     * @param type 日期类型 年/月/日
+     * @return 单条 Paste 访问次数
+     */
+    int countPastePeriod(Long key, Date date, String type);
+
+    /**
+     * 计数单条 Paste 的总访问次数
+     *
+     * @param key 主键
+     * @return 单条 Paste 总访问次数
+     */
+    int countPasteTotal(Long key);
+
+    /**
+     * 计数站点的周期（年/月/日）访问次数
+     * 用 dateConverterUtil 将日期根据 type 转换为起始时间
+     *
+     * @param date 日期
+     * @param type 日期类型 年/月/日
+     * @return Site 访问次数
+     */
+    int countSitePeriod(Date date, String type);
+
+    /**
+     * 计数站点的总访问次数
+     *
+     * @return Site 总访问次数
+     */
+    int countSiteTotal();
 }
